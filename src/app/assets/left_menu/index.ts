5de6b3f46c06a522667d5f078d2e72827dfbf242
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-14 16:07:04 -0300

    chore: Add new SVG icons for left menu and top menu

---
+++ src/app/assets/left_menu/index.ts
@@ -0,0 +1,15 @@
+// in left_menu/index.ts (for black theme)
+export { default as config } from "./white/config.svg";
+export { default as foruns } from "./white/foruns.svg";
+export { default as home } from "./white/home.svg";
+export { default as more } from "./white/more.svg";
+export { default as profile } from "./white/profile.svg";
+export { default as search } from "./white/search.svg";
+
+
+export { default as config2 } from "./black/config.svg";
+export { default as foruns2 } from "./black/foruns.svg";
+export { default as home2 } from "./black/home.svg";
+export { default as more2 } from "./black/more.svg";
+export { default as profile2 } from "./black/profile.svg";
+export { default as search2 } from "./black/search.svg";
---
+++ src/app/assets/top_menu/index.ts
@@ -0,0 +1,5 @@
+// in left_menu/index.ts
+export { default as forum_blue_logo } from "./forum_blue_logo.svg";
+export { default as music_pink_logo } from "./music_pink_logo.svg";
+export { default as playlist_purple_logo } from "./playlist_purple_logo.svg";
+export { default as podcast_green_logo } from "./podcast_green_logo.svg";
