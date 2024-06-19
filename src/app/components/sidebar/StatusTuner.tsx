1513cc6c3a9288d5de1b0d47fa53f1e5ce6bc05b
raquel-brena <raquel.lima.124@ufrn.edu.br>
2024-06-19 16:50:07 -0300

    feat: Add StatusTuner component to Sidebar

---
+++ src/app/components/sidebar/StatusTuner.tsx
@@ -0,0 +1,17 @@
+import { Photo } from "../Photo";
+import { TbMusicStar } from "react-icons/tb";
+
+export const StatusTuner = () => {
+  return (
+    <div className="md:flex relative h-[16%]  items-center  text-sm w-full p-2 border-l border-stroke hidden ">
+      <Photo size="2.5" bg="B02851" />
+      <div className="w-full px-3" >
+        <div className="flex justify-between w-full">
+          <p className="font-bold flex items-center"> melzinho da feira</p>
+          <p className="text-copacity_25 text-xs">4h</p>
+        </div>
+        <p className=" flex items-center font-light gap-1"> <TbMusicStar /> Mulher de fases - Raimundos</p>
+      </div>
+    </div>
+  );
+}
