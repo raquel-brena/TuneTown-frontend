d9f613d54c3d7c2ec0f49724fd6da2d88d6fbf08
raquel-brena <raquel.lima.124@ufrn.edu.br>
2024-06-19 16:49:37 -0300

    refactor: Update Sidebar component to include search input and StatusTuner components

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,15 +1,23 @@
 import { BoxSidebar } from "./BoxSidebar";
+import { StatusTuner } from "./StatusTuner";
 
 export const Sidebar = () => {
 
+  return (
+    <div className="md:flex flex-col items-center h-full w-[30%] gap-10 p-8 border-l border-stroke hidden ">
 
-    return (
-      <div className="md:flex flex-col h-full w-[30%] p-8 border-l border-stroke hidden ">
-        <div className="w-full  h-1/6">searc</div>
-        <div className="w-full h-[90%] md:flex flex-col gap-10">
-          <BoxSidebar />
-          <BoxSidebar />
-        </div>
+      <input className="w-[95%] h-12 bg-fume border border-stroke rounded-3xl focus:border-theme outline-none ring-0 placeholder:text-constrast placeholder:mx-3" placeholder="Buscar" />
+
+      <div className="w-full h-[90%] md:flex flex-col gap-10 ">
+        <BoxSidebar>
+          <StatusTuner />
+          <StatusTuner />
+          <StatusTuner />
+          <StatusTuner />
+          <StatusTuner />
+        </BoxSidebar>
+        <BoxSidebar> StatusTuner</BoxSidebar>
       </div>
-    );
+    </div>
+  );
 }
