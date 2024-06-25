f2df416a1f2119c8b1074f31c8fe59613216dfbd
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-25 14:26:55 -0300

    feat: Add MenuFeed component for displaying the top menu in the feed page

---
+++ src/app/components/feed/menuTop/MenuFeed.tsx
@@ -0,0 +1,38 @@
+import { MenuitemShare } from "./MenuTopitem"
+import { Photo } from "../../Photo"
+import { forum_blue_logo, music_pink_logo, playlist_purple_logo, podcast_green_logo } from "../../../assets/top_menu"
+import { Img } from "../../Img";
+import { useState } from "react";
+
+interface MenuFeedProps {
+  userAvatar?: string | null;
+  username?: string | null;
+}
+
+export const MenuFeed = ({ userAvatar, username }: MenuFeedProps) => {
+
+  const [textComment, setTextComment] = useState<string>("");
+
+  return (
+    <div
+      className="  h-[15%] w-full rounded-lg border-box relative bg-fume 
+      border border-stroke overflow-hidden"
+    >
+      <div className=" h-full px-3 md:px-6 w-full my-3 items-center justify-center ">
+        <div className="w-full items-center h-[30%] flex gap-4  ">
+          <Photo size="3" src={userAvatar} />
+
+          <div className="w-full h-full">
+            <input className="outline-none bg-copacity_25 w-full h-full rounded-md " />
+            <div className="w-full gap-4 mt-5 flex items-end justify-around ">
+              <MenuitemShare src={music_pink_logo} name="Música" />
+              <MenuitemShare src={playlist_purple_logo} name="Playlist" />
+              <MenuitemShare src={podcast_green_logo} name="Podcast" />
+              <MenuitemShare src={forum_blue_logo} name="Forum" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
