47913ea97f11b0072aa698c27c14c0bab40150a6
Raquel <raquel.lima.124@ufrn.edu.br>
2024-07-03 23:24:52 -0300

    feat: Add TextArea component

---
+++ src/app/components/TextArea.tsx
@@ -0,0 +1,33 @@
+import React, { forwardRef, useRef } from "react";
+
+interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  height?: string;
+}
+
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ label, height, ...props }, ref) => {
+    return (
+      <div className="flex flex-col w-full space-y-">
+        <label className="text-sm font-light select-none">{label}</label>
+        <textarea
+          ref={ref}
+          className={`rounded-sm ${
+            height ? `h-${height}` : `h-8`
+          } text-start appearance-none bg-slate-200 relative
+           px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] 
+           sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] 
+            bg-transparent ring-1 ring-white/30 placeholder:text-zinc-500 sm:text-sm/6 
+            text-base/6
+            block
+            focus:ring-0 
+            hover:ring-white/40
+            after:inset-0 after:rounded-lg
+           `}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
