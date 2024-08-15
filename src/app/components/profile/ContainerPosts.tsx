ccc3b523639123db680bd6e79a9ed6fc9a547190
Raquel <raquel.lima.124@ufrn.edu.br>
2024-08-15 00:16:11 -0300

    Updates

--- src/app/components/profile/ContainerPosts.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-export const ContainerPosts = ({children} : {children : JSX.Element | JSX.Element[]}) => {
-
-return (
-    <div className="items-center w-[90%]  justify-center  place-items-center flex flex-col gap-3 py-4">{children}</div>
-)
-}
--- src/infra/http/IHttpClient.ts
+++
@@ -1,5 +0,0 @@
-export default interface IHttpClient {
-    get (url: string):Promise <any>;
-    post (url: string, data: any):Promise <any>;
-    put (url: string, data: any):Promise <any>;
-}
